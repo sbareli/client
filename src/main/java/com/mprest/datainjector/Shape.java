105e0644b59881fb63f91d3ed42f1e91d3ca9036
sbareli <basixp@gmail.com>
2018-02-26 15:21:50 +0200

    Refactoring

--- src/main/java/com/mprest/datainjector/Shape.java
+++ src/main/java/com/mprest/datainjector/Shape.java
@@ -17,21 +17,21 @@ public class Shape {
         id = ID++;
         state = 0;
 
-        lines = new ArrayList<>(2);
+        edges = new ArrayList<>(2);
 
-        lines.add( (Math.random()+1.0)*100 );
-        lines.add( (Math.random()+1.0)*100 );
+        edges.add( (Math.random()+1.0)*100 );
+        edges.add( (Math.random()+1.0)*100 );
     }
 
     public String
     print() {
         return "id:" + id.toString() + ", state:" + state.toString()
-                + ", line-1:" + lines.get(0).toString() + ",line-2:" + lines.get(1).toString();
+                + ", line-1:" + edges.get(0).toString() + ",line-2:" + edges.get(1).toString();
     }
 
     private Integer id;
     private Integer state;
 
     @JsonFormat(shape = JsonFormat.Shape.ARRAY)
-    private List<Double> lines;
+    private List<Double> edges;
 }
